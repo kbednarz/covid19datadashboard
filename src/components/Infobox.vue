7bcaa04e46d7860a5ecfcf282a00b26048eea8a1
marcelinai <marcelinaiwanow@interia.pl>
2020-04-18 19:22:22 +0200

    added data to infoboxes

--- src/components/Infobox.vue
+++ src/components/Infobox.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="card" style="width: 18rem;">
+<div class="col mb-4">
+  <div class="card">
     <div class="card-body">
       <h5 class="card-title">{{ title }}</h5>
       <h6 class="card-subtitle mb-2 text-muted">{{ subtitle }}</h6>
@@ -8,6 +9,7 @@
       </p>
     </div>
   </div>
+</div>
 </template>
 
 <script>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
-    <infobox title="Ilość przypadków" text="2323232"/>
-    Dashboard
-    <line-chart :chart-data="datacollection"></line-chart>
-    <button @click="fillData()">Randomize</button>
+    <div class="mb-4">
+      <small>Dane aktualne na {{date}}</small>
+    </div>
+    <div v-if="summary" class="row row-cols-1 row-cols-md-3">
+      <infobox title="Nowe przypadki" v-bind:text="formatNumber(summary.NewConfirmed)" />
+      <infobox title="Wszystkie przypadki" v-bind:text="formatNumber(summary.TotalConfirmed)" />
+      <infobox title="Dzisiaj zmarło" v-bind:text="formatNumber(summary.NewDeaths)" />
+      <infobox title="Łącznie zmarło" v-bind:text="formatNumber(summary.TotalDeaths)" />
+      <infobox title="Dzisiaj wyzdrowiało" v-bind:text="formatNumber(summary.NewRecovered)" />
+      <infobox title="Łącznie wyzdrowiało" v-bind:text="formatNumber(summary.TotalRecovered)" />
+    </div>
+
+    <!-- <line-chart :chart-data="datacollection"></line-chart> -->
+    <!-- <button @click="fillData()">Randomize</button> -->
   </div>
 </template>
 <script>
@@ -14,40 +24,54 @@ export default {
   name: "Dashboard",
   components: {
     LineChart,
-    Infobox,
+    Infobox
   },
   data() {
     return {
-      datacollection: null,
+      date: null,
+      summary: null
+      // datacollection: null,
     };
   },
   mounted() {
-    this.fillData();
-    this.axios
-      .get("https://api.covid19api.com/")
-      .then((response) => console.log(response));
+    // this.fillData();
+    this.axios.get("https://api.covid19api.com/summary").then(r => {
+      this.date = new Date(r.data.Date);
+      this.summary = r.data.Global;
+      console.log(this.date, this.summary);
+    });
   },
   methods: {
-    fillData() {
-      this.datacollection = {
-        labels: [this.getRandomInt(), this.getRandomInt()],
-        datasets: [
-          {
-            label: "Data One",
-            backgroundColor: "#f87979",
-            data: [this.getRandomInt(), this.getRandomInt()],
-          },
-          {
-            label: "Data One",
-            backgroundColor: "#f87979",
-            data: [this.getRandomInt(), this.getRandomInt()],
-          },
-        ],
-      };
-    },
-    getRandomInt() {
-      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
-    },
-  },
+    formatNumber(num) {
+      return num;
+    }
+    // fillData() {
+    //   this.datacollection = {
+    //     labels: [this.getRandomInt(), this.getRandomInt()],
+    //     datasets: [
+    //       {
+    //         label: "Data One",
+    //         backgroundColor: "#f87979",
+    //         data: [this.getRandomInt(), this.getRandomInt()],
+    //       },
+    //       {
+    //         label: "Data One",
+    //         backgroundColor: "#f87979",
+    //         data: [this.getRandomInt(), this.getRandomInt()],
+    //       },
+    //     ],
+    //   };
+    // },
+    // getRandomInt() {
+    //   return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    // },
+  }
 };
 </script>
+
+
+<style scoped>
+.infobox {
+  margin: 1em;
+}
+</style>
